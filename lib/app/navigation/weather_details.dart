c0821e7ac850924a34dfc7d9ad2b8318404349f9
oleksandr.kozhukhar <oleksandr_kozhukhar@epam.com>
2022-01-18 16:46:43 +0200

    add blank pages

---
+++ lib/app/navigation/weather_details.dart
@@ -0,0 +1,15 @@
+class WeatherDetails extends StatefulWidget {
+  const WeatherDetails({ Key? key }) : super(key: key);
+
+  @override
+  _WeatherDetailsState createState() => _WeatherDetailsState();
+}
+
+class _WeatherDetailsState extends State<WeatherDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/app/navigation/weather_list.dart
@@ -0,0 +1,15 @@
+class WeatherList extends StatefulWidget {
+  const WeatherList({ Key? key }) : super(key: key);
+
+  @override
+  _WeatherListState createState() => _WeatherListState();
+}
+
+class _WeatherListState extends State<WeatherList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
