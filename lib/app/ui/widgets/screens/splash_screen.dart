bd0dd90689e217ceb8f514ee7277a50084589eec
oleksandr.kozhukhar <oleksandr_kozhukhar@epam.com>
2022-01-23 18:37:12 +0200

    Add theme bindings

--- lib/app/ui/widgets/screens/splash_screen.dart
+++ lib/app/ui/widgets/screens/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_search/app/bloc/theme/theme_cubit.dart';
+import 'package:weather_search/app/bloc/theme/theme_state.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,11 +15,26 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Switch themes'),
+      //   actions: [
+      //     BlocBuilder<ThemeCubit, ThemeState>(
+      //       builder: (context, state) {
+      //         return Switch(
+      //           value: state.isDarkThemeOn,
+      //           onChanged: (newValue) {
+      //             context.read<ThemeCubit>().toggleSwitch(newValue);
+      //           },
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
           Container(
-            decoration: const BoxDecoration(color: Colors.white),
+            decoration: BoxDecoration(color: Theme.of(context).backgroundColor),
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -24,24 +43,21 @@ class _SplashScreenState extends State<SplashScreen> {
                 flex: 2,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
+                  children: <Widget>[
                     CircleAvatar(
-                      backgroundColor: Colors.greenAccent,
+                      backgroundColor: Theme.of(context).primaryColor,
                       radius: 50.0,
                       child: Icon(
                         Icons.cloud_done,
-                        color: Colors.deepPurpleAccent,
+                        color: Theme.of(context).secondaryHeaderColor,
                         size: 50.0,
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(top: 10.0),
+                      padding: const EdgeInsets.only(top: 10.0),
                       child: Text(
                         'Weather Search',
-                        style: TextStyle(
-                            color: Colors.deepPurpleAccent,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0),
+                        style: Theme.of(context).textTheme.headline3,
                       ),
                     ),
                   ],
@@ -51,17 +67,15 @@ class _SplashScreenState extends State<SplashScreen> {
                 flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    CircularProgressIndicator(color: Colors.greenAccent),
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                        color: Theme.of(context).primaryColor),
                     Padding(
-                        padding: EdgeInsets.only(top: 20.0),
+                        padding: const EdgeInsets.only(top: 20.0),
                         child: Text(
                           'Loading...',
                           textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0,
-                              color: Colors.deepPurpleAccent),
+                          style: Theme.of(context).textTheme.headline2,
                         )),
                   ],
                 ),
